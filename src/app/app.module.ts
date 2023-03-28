4a173f286208101797d95322591f4c9d08b8bcd2
Lucas Cordasso <lucas.cordasso@elabbora.com.br>
2023-03-28 15:54:43 -0300

    Feito as páginas, fazendo mural

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { FormsModule } from '@angular/forms';
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
+import { PensamentoComponent } from './components/pensamentos/pensamento/pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    CriarPensamentoComponent,
+    ListarPensamentoComponent,
+    PensamentoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent {
+  pensamento = {
+    conteudo: 'I love Angular',
+    autoria: 'Nay',
+    modelo: 'modelo3'
+  }
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+}
